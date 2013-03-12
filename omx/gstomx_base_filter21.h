3f98798b99169f3adbda1190bcb4de1a6dc5a6ce
Jean-Baptiste Théou <jean-baptiste.theou@vodalys.com>
2013-03-12 17:28:37 +0100

    Add new element : Video mixer based on OMX.TI.VPSSM3.VSWMOSAIC OpenMax IL component

---
+++ omx/gstomx_base_filter21.h
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2007-2009 Nokia Corporation.
+ *
+ * Author: Felipe Contreras <felipe.contreras@nokia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef GSTOMX_BASE_FILTER21_H
+#define GSTOMX_BASE_FILTER21_H
+
+#include <gst/gst.h>
+
+G_BEGIN_DECLS
+
+#define GST_OMX_BASE_FILTER21(obj) ((GstOmxBaseFilter21 *) (obj))
+#define GST_OMX_BASE_FILTER21_TYPE (gst_omx_base_filter21_get_type ())
+#define GST_OMX_BASE_FILTER21_CLASS(obj) ((GstOmxBaseFilter21Class *) (obj))
+#define GST_OMX_BASE_FILTER21_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS ((obj), GST_OMX_BASE_FILTER21_TYPE, GstOmxBaseFilter21Class))
+
+typedef struct GstOmxBaseFilter21 GstOmxBaseFilter21;
+typedef struct GstOmxBaseFilter21Class GstOmxBaseFilter21Class;
+typedef void (*GstOmxBaseFilter21Cb) (GstOmxBaseFilter21 *self);
+typedef gboolean (*GstOmxBaseFilter21PushCb) (GstOmxBaseFilter21 *self, GstBuffer *buf);
+
+#include "gstomx_util.h"
+#include <async_queue.h>
+#include <omx_vswmosaic.h>
+
+
+struct GstOmxBaseFilter21
+{
+    GstElement element;
+    
+#define NUM_INPUTS 2
+    GstPad *sinkpad[NUM_INPUTS];
+    GstPad *srcpad;
+
+    GOmxCore *gomx;
+    GOmxPort *in_port[NUM_INPUTS];
+    GOmxPort *out_port;
+
+    char *omx_role;
+    char *omx_component;
+    char *omx_library;
+    gboolean ready;
+    GMutex *ready_lock;
+	gint in_width[NUM_INPUTS];
+	gint in_height[NUM_INPUTS];
+	gint in_stride[NUM_INPUTS];
+    gint out_width, out_height, out_stride;
+    gint framerate_num[NUM_INPUTS];
+    gint x[NUM_INPUTS];
+    gint y[NUM_INPUTS];
+    gint framerate_denom[NUM_INPUTS];
+    GstOmxBaseFilter21Cb omx_setup;
+    GstOmxBaseFilter21PushCb push_cb;
+    GstFlowReturn last_pad_push_return;
+    GstBuffer *codec_data;
+    GstClockTime duration;
+    GstClockTime last_buf_timestamp;
+    GstClockTime sink_camera_timestamp;
+	gint port_index, input_port_index[NUM_INPUTS], output_port_index;
+	int number_eos;
+
+};
+
+struct GstOmxBaseFilter21Class
+{
+    GstElementClass parent_class;
+
+    GstFlowReturn (*push_buffer) (GstOmxBaseFilter21 *self, GstBuffer *buf);
+    GstFlowReturn (*pad_chain) (GstPad *pad, GstBuffer *buf);
+    gboolean (*pad_event) (GstPad *pad, GstEvent *event);
+};
+
+GType gst_omx_base_filter21_get_type (void);
+
+G_END_DECLS
+
+#endif /* GSTOMX_BASE_FILTER2_H */
---
+++ omx/gstomx_videomixer_vodalys.c
@@ -0,0 +1,293 @@
+/*
+ * Copyright (C) 2013 Vodalys Ingénierie
+ *
+ * Author: Jean-Baptiste Théou <jean-baptiste.theou@vodalys.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+/* Video Mixer plugin, based on OMX.TI.VPSSM3.VSWMOSAIC OpenMax IL component
+ * 
+ * */
+
+#include "gstomx_videomixer_vodalys.h"
+#include "gstomx.h"
+
+#define CAMERA_INPUT_IDX 0
+#define SLIDE_INPUT_IDX 1
+
+
+#define BLACK 0x80008000
+#define GRAY  0x80808080
+#define GREEN 0x00800080
+
+#define BG_COLOR BLACK
+
+GSTOMX_BOILERPLATE (GstOmxVideoMixerVodalys, gst_omx_videomixer_vodalys, GstOmxBaseFilter21, GST_OMX_BASE_FILTER21_TYPE);
+
+
+static GstStaticPadTemplate sink_template_camera =
+        GST_STATIC_PAD_TEMPLATE ("sink_00",
+                GST_PAD_SINK,
+                GST_PAD_ALWAYS,
+                GST_STATIC_CAPS (GST_VIDEO_CAPS_YUV ( "{YUY2}" ))
+        );
+
+static GstStaticPadTemplate sink_template_slide =
+        GST_STATIC_PAD_TEMPLATE ("sink_01",
+                GST_PAD_SINK,
+                GST_PAD_ALWAYS,
+                GST_STATIC_CAPS (GST_VIDEO_CAPS_YUV ( "{YUY2}" ))
+        );
+
+static GstStaticPadTemplate src_template =
+        GST_STATIC_PAD_TEMPLATE ("src",
+                GST_PAD_SRC,
+                GST_PAD_ALWAYS,
+                GST_STATIC_CAPS (GST_VIDEO_CAPS_YUV ( "{YUY2}" ))
+        );
+
+
+static void
+type_base_init (gpointer g_class)
+{
+    GstElementClass *element_class;
+
+    element_class = GST_ELEMENT_CLASS (g_class);
+
+    {
+        GstElementDetails details;
+
+        details.longname = "OpenMAX IL videomixer";
+        details.klass = "Filter";
+        details.description = "Video Mixer element";
+        details.author = "Jean-Baptiste Theou";
+
+        gst_element_class_set_details (element_class, &details);
+    }
+
+    gst_element_class_add_pad_template (element_class,
+        gst_static_pad_template_get (&sink_template_camera));
+
+	gst_element_class_add_pad_template (element_class,
+        gst_static_pad_template_get (&sink_template_slide));
+
+    gst_element_class_add_pad_template (element_class,
+        gst_static_pad_template_get (&src_template));
+}
+
+static void
+type_class_init (gpointer g_class,
+                 gpointer class_data)
+{
+}
+
+
+static GstCaps * create_src_caps (GstOmxBaseFilter21 *omx_base)
+{
+    GstCaps *caps;    
+    GstOmxBaseFilter21 *self;
+    int width, height;
+    GstStructure *struc;
+
+    self = GST_OMX_BASE_FILTER21 (omx_base);
+    caps = gst_pad_peer_get_caps (omx_base->srcpad);
+
+    if (gst_caps_is_empty (caps))
+    {
+
+        width = 1920;
+        height = 1088;
+
+    }
+    else
+    {
+        GstStructure *s;
+
+        s = gst_caps_get_structure (caps, 0);
+
+        if (!(gst_structure_get_int (s, "width", &width) &&
+            gst_structure_get_int (s, "height", &height)))
+        {
+
+			width = 1920;
+			height = 1088;
+
+        }
+    }
+
+    caps = gst_caps_new_empty ();
+    struc = gst_structure_new (("video/x-raw-yuv"),
+            "width",  G_TYPE_INT, width,
+            "height", G_TYPE_INT, height,
+            "format", GST_TYPE_FOURCC, GST_MAKE_FOURCC ('Y', 'U', 'Y', '2'),
+            NULL);
+
+
+    gst_caps_append_structure (caps, struc);
+
+    return caps;
+}
+
+
+static void
+omx_setup (GstOmxBaseFilter21 *omx_base)
+{
+	int i;
+	GOmxCore *gomx;
+	OMX_ERRORTYPE eError = OMX_ErrorNone;
+	OMX_PARAM_BUFFER_MEMORYTYPE memTypeCfg;
+	OMX_PARAM_PORTDEFINITIONTYPE paramPort;
+	OMX_CONFIG_VSWMOSAIC_CREATEMOSAICLAYOUT  sMosaic;
+	OMX_CONFIG_VSWMOSAIC_SETBACKGROUNDCOLOR  sMosaicBg;
+	OMX_PARAM_VSWMOSAIC_MOSAIC_PERIODICITY	 sMosaicFPS;
+    GstOmxBaseFilter21 *self;
+    gomx = (GOmxCore *) omx_base->gomx;
+    self = GST_OMX_BASE_FILTER21(omx_base);
+	
+    GST_LOG_OBJECT (self, "begin");
+    /* set the output cap */
+    gst_pad_set_caps (self->srcpad, create_src_caps (omx_base));
+    /* Setting Memory type at input port to Raw Memory */
+    GST_LOG_OBJECT (self, "Setting input port to Raw memory");
+
+    _G_OMX_INIT_PARAM (&memTypeCfg);
+	memTypeCfg.eBufMemoryType = OMX_BUFFER_MEMORY_DEFAULT;
+
+	for ( i = 0; i < NUM_INPUTS; i++) {
+		memTypeCfg.nPortIndex = omx_base->in_port[i]->port_index;
+		eError = OMX_SetParameter (gomx->omx_handle, OMX_TI_IndexParamBuffMemType, &memTypeCfg);
+		if (eError != OMX_ErrorNone)
+		{
+				return;
+		}
+	}
+	
+	/* Setting Memory type at output port to Raw Memory */
+    GST_LOG_OBJECT (self, "Setting output port to Raw memory");
+    
+    memTypeCfg.nPortIndex = omx_base->out_port->port_index;
+	memTypeCfg.eBufMemoryType = OMX_BUFFER_MEMORY_DEFAULT;
+	eError = OMX_SetParameter (gomx->omx_handle, OMX_TI_IndexParamBuffMemType, &memTypeCfg);
+
+	if (eError != OMX_ErrorNone)
+	{
+		return;
+	}
+
+	GST_LOG_OBJECT (self, "Setting port definition (input)");
+
+
+	for ( i = 0; i < NUM_INPUTS; i++) {
+		/* set input height/width and color format */
+		G_OMX_PORT_GET_DEFINITION (omx_base->in_port[i], &paramPort);
+		paramPort.format.video.nFrameWidth  = self->in_width[i]; 
+		paramPort.format.video.nFrameHeight = self->in_height[i];
+		/* swmosaic is connceted to scalar, whose stride is different than width*/
+		paramPort.format.video.nStride = self->in_stride[i];
+		paramPort.format.video.eCompressionFormat = OMX_VIDEO_CodingUnused;
+		paramPort.format.video.eColorFormat = OMX_COLOR_FormatYCbYCr;
+		paramPort.nBufferSize = (paramPort.format.video.nStride * paramPort.format.video.nFrameHeight);
+
+		paramPort.nBufferAlignment = 0;
+		paramPort.bBuffersContiguous = 0;
+	
+		G_OMX_PORT_SET_DEFINITION (omx_base->in_port[i], &paramPort);
+		g_omx_port_setup (omx_base->in_port[i], &paramPort);
+	}
+	
+	G_OMX_PORT_GET_DEFINITION (omx_base->out_port, &paramPort);
+	paramPort.format.video.nFrameWidth  = self->out_width;
+	paramPort.format.video.nFrameHeight = self->out_height;
+	paramPort.format.video.eCompressionFormat = OMX_VIDEO_CodingUnused;
+	paramPort.format.video.eColorFormat = OMX_COLOR_FormatYCbYCr;
+	paramPort.nBufferAlignment = 0;
+	paramPort.bBuffersContiguous = 0;
+	paramPort.nBufferCountActual = 5;
+	paramPort.format.video.nStride = self->out_stride;
+	paramPort.nBufferSize = paramPort.format.video.nStride * paramPort.format.video.nFrameHeight;
+	
+	G_OMX_PORT_SET_DEFINITION (omx_base->out_port, &paramPort);
+	g_omx_port_setup (omx_base->out_port, &paramPort);
+	
+	_G_OMX_INIT_PARAM (&sMosaic);
+	sMosaic.nPortIndex  = OMX_VSWMOSAIC_OUTPUT_PORT_START_INDEX;
+  
+	sMosaic.nOpWidth    = self->out_width;  /* Width in pixels  */
+	sMosaic.nOpHeight   = self->out_height; /* Height in pixels */
+	sMosaic.nOpPitch    = self->out_stride; /* Pitch in bytes   */
+
+	sMosaic.nNumWindows = 2;
+	sMosaic.sMosaicWinFmt[0].dataFormat = OMX_COLOR_FormatYCbYCr;
+	sMosaic.sMosaicWinFmt[0].nPortIndex = 0;
+	sMosaic.sMosaicWinFmt[0].pitch[0]   =  self->in_stride[0];
+	sMosaic.sMosaicWinFmt[0].winStartX  = self->x[0];
+	sMosaic.sMosaicWinFmt[0].winStartY  = self->y[0];
+	sMosaic.sMosaicWinFmt[0].winWidth   = self->in_width[0];
+	sMosaic.sMosaicWinFmt[0].winHeight  = self->in_height[0];
+	
+	sMosaic.sMosaicWinFmt[1].dataFormat = OMX_COLOR_FormatYCbYCr;
+	sMosaic.sMosaicWinFmt[1].nPortIndex = 1;
+	sMosaic.sMosaicWinFmt[1].pitch[0]   = self->in_stride[1];
+	sMosaic.sMosaicWinFmt[1].winStartX  = self->x[1];
+	sMosaic.sMosaicWinFmt[1].winStartY  = self->y[1];
+	sMosaic.sMosaicWinFmt[1].winWidth   = self->in_width[1];
+	sMosaic.sMosaicWinFmt[1].winHeight  = self->in_height[1];
+	
+	eError = OMX_SetConfig (gomx->omx_handle, 
+							OMX_TI_IndexConfigVSWMOSAICCreateMosaicLayout, 
+							&sMosaic);
+	if (eError != OMX_ErrorNone)
+	{
+			printf("Error during Layout settings\n");
+	}
+	_G_OMX_INIT_PARAM(&sMosaicBg);
+	sMosaicBg.nPortIndex  = OMX_VSWMOSAIC_OUTPUT_PORT_START_INDEX;
+	sMosaicBg.uColor = BG_COLOR;
+	eError = OMX_SetConfig (gomx->omx_handle, 
+							OMX_TI_IndexConfigVSWMOSAICSetBackgroundColor, 
+							&sMosaicBg);
+	if (eError != OMX_ErrorNone)
+	{
+			printf("Error during background\n");
+	}
+	
+// FPS settings didn't work actually 
+#if 1
+	_G_OMX_INIT_PARAM(&sMosaicFPS);
+	sMosaicFPS.nPortIndex  = OMX_VSWMOSAIC_OUTPUT_PORT_START_INDEX;
+	sMosaicFPS.nFps		   = 60;				
+	eError = OMX_SetConfig (gomx->omx_handle, 
+							OMX_TI_IndexParamVSWMOSAICMosaicPeriodicity, 
+							&sMosaicFPS);
+	if (eError != OMX_ErrorNone)
+	{
+			printf("Error during FPS settings : %s\n",g_omx_error_to_str (eError));
+	}
+#endif
+}
+
+
+static void
+type_instance_init (GTypeInstance *instance,
+                    gpointer g_class)
+{
+    GstOmxBaseFilter21 *omx_base;
+
+    omx_base = GST_OMX_BASE_FILTER21 (instance);
+	omx_base->omx_setup = omx_setup;
+    GST_DEBUG_OBJECT (omx_base, "start");
+}
