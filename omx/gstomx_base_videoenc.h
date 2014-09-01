fad0a7e18064379b182f4a763e7dde650a4a6fa5
Eugenia Guzman <eugenia.guzman@ridgerun.com>
2014-09-01 14:41:55 -0600

    Adding h264 interlaced encoding support

--- omx/gstomx_base_videoenc.h
+++ omx/gstomx_base_videoenc.h
@@ -45,6 +45,7 @@ struct GstOmxBaseVideoEnc
     GstOmxBaseFilterCb omx_setup;
 
     gint rowstride;     /**< rowstride of input buffer */
+    gboolean interlaced;
 };
 
 struct GstOmxBaseVideoEncClass
